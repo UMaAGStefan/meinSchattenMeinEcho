18ae1cc99fda435aa8ccba62aa90b1229859550d
UMaAGStefan <sb.umaag@umwelt-management.de>
2024-01-05 07:47:11 +0100

    Zusätzliche Schallmode Optimierung

--- main.py
+++ main.py
@@ -5,25 +5,26 @@ import time
 import shadow
 import noise
 start_time_all = time.time()
-fileName = r"C:\Users\sb.umaag\OneDrive - Umwelt Management AG UMaAG\Dokumente\WindPRO Data\Projects\BWE Ringversuch\2023 Schall und Schattenringversuch\BWE_RV-Schall+Schatten_Eingangsdaten_angepasst_UMasEcho.xlsx"
+fileName = r"C:\Users\sb.umaag\OneDrive - Umwelt Management AG UMaAG\Dokumente\GitHub\meinSchattenMeinEcho\example\yyyymmdd_EINGANG_meinSchattenMeinEcho_Projekt.xlsx"
 crsInp = 'EPSG:25832'
 #df_wea = pd.read_excel(fileName,sheet_name='WEA',usecols=['User label', 'Object type', 'Ost ', 'Nord ', 'Z', 'Object description','Hub height', 'NachtBetrieb'])
 df_wea = pd.read_excel(fileName,sheet_name='WEA')
+df_wea = df_wea[df_wea['Object type'] != 0]
+df_wea.loc[df_wea['Object type']=='New','Object type']= 'Neue WEA'
 gdf_wea = geopandas.GeoDataFrame(df_wea.drop(columns=['Ost ', 'Nord ']),
                                 geometry=geopandas.points_from_xy(df_wea['Ost '],df_wea['Nord ']),
                                  crs=crsInp)
-
 df_io = pd.read_excel(fileName,sheet_name='Schall',usecols=['IRW', 'Description', 'Ost ', 'Nord ', 'Z', 'User label'])
 df_sr = pd.read_excel(fileName,sheet_name='Schatten')
 gdf_sr = geopandas.GeoDataFrame(df_sr.drop(columns=['Ost ', 'Nord ']),
                                 geometry=geopandas.points_from_xy(df_sr['Ost '], df_sr['Nord ']),
                                 crs=crsInp)
-outfile = fileName.replace('.xlsx','Outfile.xlsx')
+outfile = fileName.replace('.xlsx','_Outfile.xlsx')
 gdf_wea.to_excel(outfile,index=None,sheet_name='WEA Eingangsdaten')
 
 # # # # # # SHADOW # # # # # # #
 gdf_srVB, gdf_srZB, gdf_srGB = shadow.shadowFlicker(gdf_wea,gdf_sr,outfile)
 # # # # # # NOISE # # # # # # #
+df_io, df_wea = noise.schallKonzeptFortePiano(df_wea,df_io,fileName)
+df_wea = noise.ExtraRundeSchallModes(df_wea,df_io,fileName)
 df_io = noise.calcAndEvaluateNoise(df_wea,df_io,outfile)
-
-print(df_io)
--- noise.py
+++ noise.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
-
-
+from itertools import product
+import time
 def calcVBZB(df_wea,df_io,ind):
     df_calcs = pd.DataFrame(columns=['ZB', 'VB', 'L WEA', 'D', 'A', 'Adiv', 'Aatm', 'Agr', 'Abar', 'Cmet','Dc'])
     df_calcs['D']=(((df_wea.loc[:,'Ost ']-df_io.loc[ind,'Ost '])**2+
@@ -9,14 +9,15 @@ def calcVBZB(df_wea,df_io,ind):
                          (df_io.loc[ind,'Z']-df_wea.loc[:,'Z']+df_wea.loc[:,'Hub height'])**2)**(1/2)).astype('float')
     df_calcs['Adiv'] = 20*np.log10(df_calcs['D']/1)+11
     df_calcs['Aatm'] =df_wea['LatDW']-\
-                (10*np.log10(10**((df_wea[63]-(0.1*df_calcs['D']/1000))/10)+10**((df_wea[125]-(0.4*df_calcs['D']/1000))/10)+
-                           10**((df_wea[250]-(1*df_calcs['D']/1000))/10)+10**((df_wea[500]-(1.9*df_calcs['D']/1000))/10)+
-                           10**((df_wea[1000]-(3.7*df_calcs['D']/1000))/10)+10**((df_wea[2000]-(9.7*df_calcs['D']/1000))/10)+
-                           10**((df_wea[4000]-(32.8*df_calcs['D']/1000))/10)+10**((df_wea[8000]-(117*df_calcs['D']/1000))/10)))
+                (10*np.log10(10**((df_wea[63].astype(float)-(0.1*df_calcs['D']/1000))/10)+10**((df_wea[125].astype(float)-(0.4*df_calcs['D']/1000))/10)+
+                           10**((df_wea[250].astype(float)-(1*df_calcs['D']/1000))/10)+10**((df_wea[500].astype(float)-(1.9*df_calcs['D']/1000))/10)+
+                           10**((df_wea[1000].astype(float)-(3.7*df_calcs['D']/1000))/10)+10**((df_wea[2000].astype(float)-(9.7*df_calcs['D']/1000))/10)+
+                           10**((df_wea[4000].astype(float)-(32.8*df_calcs['D']/1000))/10)+10**((df_wea[8000].astype(float)-(117*df_calcs['D']/1000))/10)))
     df_calcs['Agr'] = -3.0
     df_calcs['Dc'] = 0.0
     df_calcs['Abar'] = 0
     df_calcs['Cmet'] = 0
+    # Alternatives Verfahren für alle WEA mit NH kleiner 50m
     for wea_ind in df_wea[df_wea['Hub height']<=50].index:
         df_calcs.loc[wea_ind,'Aatm'] = df_wea.loc[wea_ind,'LatDW'] - \
                                        10*np.log10(10**((df_wea.loc[wea_ind,'LatDW']-
@@ -31,8 +32,8 @@ def calcVBZB(df_wea,df_io,ind):
     df_calcs['A'] = (df_calcs.loc[:,'Adiv':'Cmet']).sum(axis=1)
     df_calcs['L WEA'] = df_wea['LatDW']-df_calcs['A']+df_calcs['Dc']+df_wea['deltaL']
     df_calcs['Object type']=df_wea['Object type']
-    WeaVB = 0
-    GewVB = 0
+    WeaVB = -100
+    GewVB = -100
     if len(df_calcs.loc[df_calcs['Object type']=='Existierende WEA','L WEA'])>0:
         WeaVB = 10*np.log10((10**(df_calcs.loc[df_calcs['Object type']=='Existierende WEA','L WEA']/10)).sum(axis=0))
     if len(df_calcs.loc[df_calcs['Object type']=='Gewerbliche Vorbelastung','L WEA'])>0:
@@ -42,10 +43,10 @@ def calcVBZB(df_wea,df_io,ind):
            10*np.log10((10**(df_calcs.loc[df_calcs['Object type']=='Neue WEA','L WEA']/10)).sum(axis=0)),\
             df_calcs# VB,ZB
 
-def calcAndEvaluateNoise(df_wea,df_io,outfile):
+def calcAndEvaluateNoise(df_wea,df_io,outfile=None):
     # Falls kein Oktavband angegeben wird das LAI Oktavband verwendet
-    if len(df_wea[df_wea['LatDW'] != 10 * np.log10((10 ** (df_wea.loc[:, 63:8000] / 10)).sum(axis=1))])>0:
-        for ind_wea in df_wea[df_wea['LatDW'] != 10 * np.log10((10 ** (df_wea.loc[:, 63:8000] / 10)).sum(axis=1))].index:
+    if len(df_wea[abs(df_wea['LatDW'] - 10 * np.log10((10 ** (df_wea.loc[:, 63:8000] / 10)).sum(axis=1)))>1])>0:
+        for ind_wea in df_wea[abs(df_wea['LatDW'] - 10 * np.log10((10 ** (df_wea.loc[:, 63:8000] / 10)).sum(axis=1)))>1].index:
             if (df_wea.loc[ind_wea,'Object type']=='Neue WEA') or\
                 (df_wea.loc[ind_wea, 'Object type'] == 'Existierende WEA'):
                 df_wea.loc[ind_wea, 63:8000] = df_wea.loc[ind_wea, 'LatDW']+[-20.3,-11.9,-7.7,-5.5,-6.0,-8.0,-12.0,-20.0]
@@ -61,8 +62,7 @@ def calcAndEvaluateNoise(df_wea,df_io,outfile):
             df_io.loc[ind_io, 'Bewertung'] = 'Einwirkbereich'
         elif df_io.loc[ind_io, 'GB'] - 0.45 < df_io.loc[ind_io, 'IRW']:
             df_io.loc[ind_io, 'Bewertung'] = 'GB kleiner IRW'
-        elif df_io.loc[ind_io, 'GB'] - 1.45 < df_io.loc[ind_io, 'IRW'] and df_io.loc[ind_io, 'VB'] + 10 > df_io.loc[
-            ind_io, 'IRW']:
+        elif df_io.loc[ind_io, 'GB'] - 1.45 < df_io.loc[ind_io, 'IRW'] and df_io.loc[ind_io, 'VB'] + 10 > df_io.loc[ind_io, 'IRW']:
             df_io.loc[ind_io, 'Bewertung'] = 'GB 1dB über IRW'
         elif all(df_calcs.loc[df_calcs['Object type'] == 'Neue WEA', 'L WEA'] + 10 < df_io.loc[ind_io, 'IRW']):
             df_io.loc[ind_io, 'Bewertung'] = 'Einzelbeitrag 10dB unter IRW'
@@ -70,17 +70,188 @@ def calcAndEvaluateNoise(df_wea,df_io,outfile):
             df_io.loc[ind_io, 'Bewertung'] = 'Nicht ok'
     df_wea.name = 'Windkraftanlagen'
     df_io.name = 'Bewertung Schall Immissionsorte'
-    with pd.ExcelWriter(outfile, engine='openpyxl', mode='a', if_sheet_exists='overlay') as writer:
-        for df in [df_wea, df_io]:
-            sheet = writer.sheets['Noise'] if 'Noise' in writer.sheets else None
-            if sheet is None:
-                # Das Tabellenblatt existiert noch nicht, erstellen Sie es
-                df.to_excel(writer, sheet_name='Noise', index=None,startrow=1)
-                sheet = writer.sheets['Noise']
-            else:
-                # Das Tabellenblatt existiert bereits, fügen Sie die Daten darunter hinzu
-                sheet.append([])
-                sheet.append([df.name])  # Fügen Sie die Überschrift in eine neue Zeile ein
-                df.to_excel(writer, sheet_name='Noise', index=None,
-                             startrow=sheet.max_row)
+    if outfile!=None:
+        with pd.ExcelWriter(outfile, engine='openpyxl', mode='a', if_sheet_exists='overlay') as writer:
+            for df in [df_wea, df_io]:
+                sheet = writer.sheets['Noise'] if 'Noise' in writer.sheets else None
+                if sheet is None:
+                    # Das Tabellenblatt existiert noch nicht, erstellen Sie es
+                    df.to_excel(writer, sheet_name='Noise', index=None,startrow=1)
+                    sheet = writer.sheets['Noise']
+                else:
+                    # Das Tabellenblatt existiert bereits, fügen Sie die Daten darunter hinzu
+                    sheet.append([])
+                    sheet.append([df.name])  # Fügen Sie die Überschrift in eine neue Zeile ein
+                    df.to_excel(writer, sheet_name='Noise', index=None,
+                                 startrow=sheet.max_row)
     return df_io
+
+def schallKonzeptFortePiano(df_wea,df_io, fileName):
+    df_inp = pd.read_excel(fileName, sheet_name='WEASchallDaten', header=None, index_col=0)
+
+    for ind in df_wea['NachtBetrieb'].isnull().index:
+        start_index = df_inp.index.get_loc(df_wea.loc[ind, 'Object description'])
+        df_wea.loc[ind, 'NachtBetrieb'] = df_inp.iloc[start_index + 1, :10].name
+        df_wea.loc[ind, 63:'deltaL'] = df_inp.iloc[start_index + 1, :10].values
+        df_wea.loc[ind, 'NB kW'] = df_inp.iloc[start_index + 1, -1]
+    df_io = calcAndEvaluateNoise(df_wea, df_io,outfile=None)
+    df_io['Abstand ZB IRW'] = df_io['ZB'] - df_io['IRW']
+    # # Hier kommt noch eine While abfrage
+    while any(df_io['Bewertung'] == 'Nicht ok'):
+        ind_io = df_io[df_io['Bewertung'] == 'Nicht ok']['Abstand ZB IRW'].nlargest(1).index.values[0]
+        WeaVB, GewVB, ZB, df_calcs = calcVBZB(df_wea, df_io, ind_io)
+
+        wea_ind = df_calcs[df_calcs['Object type'] == 'Neue WEA']['L WEA'].nlargest(1).index.values[0]
+        start_index = df_inp.index.get_loc(df_wea.loc[wea_ind, 'Object description'])
+        aus_index = start_index + np.where(pd.isna(df_inp.index[start_index:]))[0][0]
+        mode_index = df_inp.iloc[start_index:aus_index].index.get_loc(df_wea.loc[wea_ind, 'NachtBetrieb'])
+        if mode_index < aus_index - 1:
+            # Lauteste Anlage ein Mode leiser
+            df_wea.loc[wea_ind, 'NachtBetrieb'] = df_inp.iloc[mode_index + 1].name
+            df_wea.loc[wea_ind, 63:'deltaL'] = df_inp.iloc[mode_index + 1, :10].values
+            df_wea.loc[wea_ind, 'NB kW'] = df_inp.iloc[mode_index + 1, -1]
+        # Falls lauteste Anlage bereits im kleinsten Mode
+        elif mode_index == aus_index - 1 and (len(df_wea['Object type'] == 'Neue WEA') > 2) and \
+                df_calcs[df_calcs['Object type'] == 'Neue WEA']['L WEA'].nlargest(2).diff().values[1] < 3:
+            # zweit Lauteste WEA
+            wea_ind = df_calcs[df_calcs['Object type'] == 'Neue WEA']['L WEA'].nlargest(2).index.values[1]
+            start_index = df_inp.index.get_loc(df_wea.loc[wea_ind, 'Object description'])
+            aus_index = start_index + np.where(pd.isna(df_inp.index[start_index:]))[0][0]
+            mode_index = df_inp.iloc[start_index:aus_index].index.get_loc(df_wea.loc[wea_ind, 'NachtBetrieb'])
+            df_wea.loc[wea_ind, 'NachtBetrieb'] = df_inp.iloc[mode_index + 1].name
+            df_wea.loc[wea_ind, 63:'deltaL'] = df_inp.iloc[mode_index + 1, :10].values
+            df_wea.loc[wea_ind, 'NB kW'] = df_inp.iloc[mode_index + 1, -1]
+            if mode_index < aus_index - 1:
+                # zweite lauteste Anlage einen Mode leiser
+                df_wea.loc[wea_ind, 'NachtBetrieb'] = df_inp.iloc[mode_index + 1].name
+                df_wea.loc[wea_ind, 63:'deltaL'] = df_inp.iloc[mode_index + 1, :10].values
+                df_wea.loc[wea_ind, 'NB kW'] = df_inp.iloc[mode_index + 1, -1]
+            # Falls auch zweit lauteste im kleinsten Mode
+            elif (mode_index == aus_index - 1) and (len(df_wea['Object type'] == 'Neue WEA') > 3) and \
+                    df_calcs[df_calcs['Object type'] == 'Neue WEA']['L WEA'].nlargest(3).diff().values[2] < 3:
+                # dritte Anlage ein Mode leister
+                wea_ind = df_calcs[df_calcs['Object type'] == 'Neue WEA']['L WEA'].nlargest(3).index.values[1]
+                start_index = df_inp.index.get_loc(df_wea.loc[wea_ind, 'Object description'])
+                aus_index = start_index + np.where(pd.isna(df_inp.index[start_index:]))[0][0]
+                mode_index = df_inp.iloc[start_index:aus_index].index.get_loc(df_wea.loc[wea_ind, 'NachtBetrieb'])
+                df_wea.loc[wea_ind, 'NachtBetrieb'] = df_inp.iloc[mode_index + 1].name
+                df_wea.loc[wea_ind, 63:'deltaL'] = df_inp.iloc[mode_index + 1, :10].values
+                df_wea.loc[wea_ind, 'NB kW'] = df_inp.iloc[mode_index + 1, -1]
+
+                if mode_index < aus_index - 1:
+                    # dritt lauteste Anlage einen Mode leiser
+                    df_wea.loc[wea_ind, 'NachtBetrieb'] = df_inp.iloc[mode_index + 1].name
+                    df_wea.loc[wea_ind, 63:'deltaL'] = df_inp.iloc[mode_index + 1, :10].values
+                    df_wea.loc[wea_ind, 'NB kW'] = df_inp.iloc[mode_index + 1, -1]
+                else:
+                    wea_ind = df_calcs[df_calcs['Object type'] == 'Neue WEA']['L WEA'].idxmax()
+                    df_wea.loc[wea_ind, 'NachtBetrieb'] = 'Aus'
+                    df_wea.loc[wea_ind, 63:'deltaL'] = 0
+                    df_wea.loc[wea_ind, 'NB kW'] = 0
+            else:
+                wea_ind = df_calcs[df_calcs['Object type'] == 'Neue WEA']['L WEA'].idxmax()
+                df_wea.loc[wea_ind, 'NachtBetrieb'] = 'Aus'
+                df_wea.loc[wea_ind, 63:'deltaL'] = 0
+                df_wea.loc[wea_ind, 'NB kW'] = 0
+        else:
+            wea_ind = df_calcs[df_calcs['Object type'] == 'Neue WEA']['L WEA'].idxmax()
+            df_wea.loc[wea_ind, 'NachtBetrieb'] = 'Aus'
+            df_wea.loc[wea_ind, 63:'deltaL'] = 0
+            df_wea.loc[wea_ind, 'NB kW'] = 0
+        df_io = calcAndEvaluateNoise(df_wea, df_io, outfile=None)
+        df_io['Abstand ZB IRW'] = df_io['ZB'] - df_io['IRW']
+    df_wea
+    return df_io, df_wea
+
+def ExtraRundeSchallModes(df_wea,df_io,fileName):
+    # # ExtraRundeSchallModes
+    df_inp = pd.read_excel(fileName, sheet_name='WEASchallDaten', header=None, index_col=0)
+
+    df_wea_allModes = pd.DataFrame(columns=df_wea.columns[:18], index=pd.MultiIndex.from_tuples([], names=(u'WEA', u'Mode')))
+    for wea_ind in df_wea.loc[df_wea['Object type'] == 'Neue WEA'].index:
+        start_index = df_inp.index.get_loc(df_wea.loc[wea_ind, 'Object description'])
+        aus_index = start_index + np.where(pd.isna(df_inp.index[start_index:]))[0][0]
+        df_modes = df_inp.iloc[start_index+1:aus_index]
+        if df_modes.index.get_loc(df_wea.loc[wea_ind, 'NachtBetrieb'])==0:
+            mode_index_start = 0
+            mode_index_end = 2
+        elif df_modes.index.get_loc(df_wea.loc[wea_ind, 'NachtBetrieb'])==len(df_modes):
+            mode_index_start = len(df_modes)-2
+            mode_index_end = len(df_modes)
+        elif df_modes.index.get_loc(df_wea.loc[wea_ind, 'NachtBetrieb']) < aus_index  and \
+            df_modes.index.get_loc(df_wea.loc[wea_ind, 'NachtBetrieb']) > start_index:
+            mode_index_start = df_modes.index.get_loc(df_wea.loc[wea_ind, 'NachtBetrieb'])-1
+            mode_index_end = df_modes.index.get_loc(df_wea.loc[wea_ind, 'NachtBetrieb'])+2
+        for mode, ind_mode in zip(df_modes.iloc[mode_index_start: mode_index_end].values,
+                                  df_modes.iloc[mode_index_start: mode_index_end].index):
+            df_wea_allModes.loc[(df_wea.loc[wea_ind, 'User label'],ind_mode),:'Hub height']=df_wea.loc[wea_ind,:'Hub height']
+            df_wea_allModes.loc[(df_wea.loc[wea_ind, 'User label'],ind_mode),'NachtBetrieb'] = ind_mode
+            df_wea_allModes.loc[(df_wea.loc[wea_ind, 'User label'],ind_mode),63:'deltaL'] = mode.astype(float)[:-1]
+            df_wea_allModes.loc[(df_wea.loc[wea_ind, 'User label'], ind_mode), 'kW'] = mode.astype(float)[-1]
+    data1 = pd.DataFrame(df_wea_allModes.values,
+                         index=df_wea_allModes.index.map('_'.join),
+                         columns=df_wea_allModes.columns)
+
+    df_io_allModes = pd.DataFrame(index=data1.index,columns=df_io['User label'])
+
+    for ind_io in df_io.index:
+        df_io.loc[ind_io,'WeaVB'],\
+        df_io.loc[ind_io, 'GewVB'] = \
+            calcVBZB(df_wea.loc[df_wea['Object type'] != 'Neue WEA'], df_io,ind_io)[:2]
+        df_io_allModes.iloc[:, ind_io] = calcVBZB(data1, df_io, ind_io)[3]['L WEA']
+    temp = calcVBZB(data1, df_io, ind_io)[3]
+    df_io.loc[:,'VB'] = 10 * np.log10((10 ** (df_io.loc[:,['WeaVB', 'GewVB']] / 10)).sum(axis=1).astype(float))
+
+    df_io.loc[:, 'IRW Calc'] = df_io.loc[:,'IRW']
+    # Für Bedingung falls Vorbelastung vorhanden +1dB über Richtwert möglich
+    if not df_io[df_io.loc[:,'VB']>df_io.loc[:,'IRW']-10].empty:
+        df_io.loc[df_io.loc[:,'VB']>df_io.loc[:,'IRW']-10,'IRW Calc']=\
+            df_io.loc[df_io.loc[:,'VB']>df_io.loc[:,'VB']>df_io.loc[:,'IRW']-10,'IRW']+1
+    df_io = df_io.set_index('User label')
+    # possible combinations modes**wea
+    start_time = time.time()
+    df_io_allModes_calcs = 10**(df_io_allModes/10)
+    df_io_vb_calcs = 10**(df_io.loc[:,'VB']/10).values
+
+    bestCombinations = pd.DataFrame(index=range(0,len(df_wea)),columns=range(0,6))
+    bestCombinations.loc['kW',:]=10
+
+    prefixes = [f'W{i:d}_' for i in range(1, 12)]
+
+    possibleCombinations = len(list(product(*[df_io_allModes[df_io_allModes.index.str.startswith(prefix)].index for prefix in prefixes])))
+    max_iterations = 100000
+    if possibleCombinations>max_iterations:
+        print('Iterationen reduziert')
+        print(f"Länge des Produkts: {possibleCombinations}")
+    print('geschätze Dauer ' + str(round(possibleCombinations*0.000987886)) + ' s')
+    print('geschätze Dauer ' + str(round(possibleCombinations*0.000987886/60,1)) + ' min')
+    print('geschätze Dauer ' + str(round(possibleCombinations*0.000987886/60/60,1)) + ' h')
+
+    for i, combination in enumerate(product(*[df_io_allModes[df_io_allModes.index.str.startswith(prefix)].index for prefix in prefixes])):
+
+        #combi_ZB = 10*np.log10((10**(df_io_allModes.loc[list(combination)]/10.0)).sum(axis=0).astype(float))
+        combi_ZB = 10*np.log10((df_io_allModes_calcs.loc[list(combination)]).sum(axis=0).astype(float))
+        #combi_GB = 10*np.log10(10**(df_io.loc[:,'VB']/10).values+(10**(combi_ZB/10.0)).astype(float))
+        combi_GB = 10*np.log10(df_io_vb_calcs+(10**(combi_ZB/10.0)).astype(float))
+        df_io.loc[:, 'Bewertung'] = 0
+        df_io.loc[combi_GB-0.45<df_io['IRW Calc'].values,'Bewertung']=1
+        #df_io.loc[(combi_GB-1.45<df_io['IRW'].values)&(df_io['VB'].values-1.45>df_io['IRW'].values),'Bewertung']=1
+        #df_io.loc[(df_io_allModes.loc[list(combination)]-df_io['IRW'].values+10>0).all(axis='rows'),'Bewertung']=1
+        if all(df_io['Bewertung']!=0):
+            if data1.loc[list(combination),'kW'].sum()>bestCombinations.loc['kW'].min():
+                bestCombinations.loc[:len(df_wea)-1,bestCombinations.loc['kW'].idxmin()] = combination
+                bestCombinations.loc['kW',bestCombinations.loc['kW'].idxmin()] = data1.loc[list(combination),'kW'].sum()
+        # Überprüfe, ob die maximale Anzahl von Iterationen erreicht ist
+        if i + 1 == max_iterations:
+            break
+        if i%1000==0:
+            print("Fortschritt: {:.1f}%".format(i/possibleCombinations*100))
+
+    print(i)
+    print("--- %s seconds for ---" % (time.time() - start_time))
+    print(bestCombinations)
+    #combination=list(prefixes+df_wea['NachtBetrieb'])
+    df_wea['NB kW'].sum()
+    df_wea.loc[:,'NachtBetrieb':'deltaL'] = data1.loc[list(bestCombinations.loc[:,bestCombinations.loc['kW'].idxmax()])[:-1],'NachtBetrieb':'deltaL'].values
+    df_wea.loc[:,'NB kW'] = data1.loc[list(bestCombinations.loc[:,bestCombinations.loc['kW'].idxmax()])[:-1],'kW'].values
+    return df_wea
